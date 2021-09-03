1036743c4716893e2ee31e61d4f94c25c9e4ffd3
DavidSprla <sprla21@seznam.cz>
2021-09-03 21:51:16 +0200

    Update

--- src/DavidSprla-root-config.js
+++ src/DavidSprla-root-config.js
@@ -1,19 +1,10 @@
 import { registerApplication, start } from "single-spa";
-import * as isActive from "./activity-functions";
-
-// registerApplication({
-//   name: "@single-spa/welcome",
-//   app: () =>
-//     System.import(
-//       "https://unpkg.com/single-spa-welcome/dist/single-spa-welcome.js"
-//     ),
-//   activeWhen: ["/"],
-// });
+import * as location from "./locations";
 
 registerApplication({
   name: "@DavidSprla/navbar",
   app: () => System.import("@DavidSprla/navbar"),
-  activeWhen: isActive.navbar,
+  activeWhen: location.navbar,
 	customProps: {
 		domElement: document.getElementById('navbar')
 	}
@@ -22,7 +13,7 @@ registerApplication({
 registerApplication({
   name: "@DavidSprla/movies",
   app: () => System.import("@DavidSprla/movies"),
-  activeWhen: isActive.movies,
+  activeWhen: location.movies,
 	customProps: {
 		domElement: document.getElementById('movies')
 	}
@@ -31,7 +22,7 @@ registerApplication({
 registerApplication({
   name: "@DavidSprla/favourite",
   app: () => System.import("@DavidSprla/favourite"),
-  activeWhen: isActive.favourite,
+  activeWhen: location.favourite,
 	customProps: {
 		domElement: document.getElementById('favourite')
 	}
